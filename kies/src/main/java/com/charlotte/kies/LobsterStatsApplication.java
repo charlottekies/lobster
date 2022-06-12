8bf5c1fdb641a444da15954555155c02fbfc3f14
Charlotte Kies <chopekies@gmail.com>
2022-06-12 14:05:19 -0400

    create.sh file added

---
+++ kies/src/main/java/com/charlotte/kies/LobsterStatsApplication.java
@@ -0,0 +1,13 @@
+package com.charlotte.kies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobsterStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobsterStatsApplication.class, args);
+	}
+
+}
---
+++ kies/src/test/java/com/charlotte/kies/LobsterStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlotte.kies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobsterStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
