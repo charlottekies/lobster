5ca591c29bc5c3c4c78d17f45cbb04cdccf9d4cc
Charlotte Kies <chopekies@gmail.com>
2022-06-20 18:54:24 -0400

    generated a chart

--- frontend/lobster-ui/src/App.js
+++ frontend/lobster-ui/src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import axios from "axios";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 
 const http = axios.create({
   baseURL: "http://localhost:8080",
@@ -10,6 +12,18 @@ const http = axios.create({
 class App extends React.Component {
   state = {
     lobsterData: [],
+    data: {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+      datasets: [
+        {
+          label: "First dataset",
+          data: [33, 53, 85, 41, 44, 65],
+          fill: true,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+        },
+      ],
+    },
   };
 
   // on page load
@@ -33,13 +47,13 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <div className="App-intro">
             <h2>Lobster Prices</h2>
+            <Line data={this.state.data} />
             {lobsterData.map((array) => (
               <div key={array.date}>
                 {array.date} <span>value: </span>
                 {array.value}
               </div>
             ))}
-            {/* <div>{lobsterData.observations[0]}</div> */}
           </div>
         </header>
       </div>
