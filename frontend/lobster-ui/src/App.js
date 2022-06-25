18ea6d16e2a466950a3902f59f5cb5e3ae9876c3
Charlotte Kies <chopekies@gmail.com>
2022-06-25 12:58:54 -0400

    separated dates and values into state arrays

--- frontend/lobster-ui/src/App.js
+++ frontend/lobster-ui/src/App.js
@@ -2,8 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import axios from "axios";
-import { Line } from "react-chartjs-2";
-import Chart from "chart.js/auto";
+// import { Line } from "react-chartjs-2";
 
 const http = axios.create({
   baseURL: "http://localhost:8080",
@@ -12,22 +11,12 @@ const http = axios.create({
 class App extends React.Component {
   state = {
     lobsterData: [],
-    data: {
-      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-      datasets: [
-        {
-          label: "First dataset",
-          data: [33, 53, 85, 41, 44, 65],
-          fill: true,
-          backgroundColor: "rgba(75,192,192,0.2)",
-          borderColor: "rgba(75,192,192,1)",
-        },
-      ],
-    },
+    prices: [],
+    dates: [],
   };
 
   // on page load
-  async componentDidMount() {
+  componentDidMount() {
     let body = "";
     http
       .get("/lobsters/historical-price-data")
@@ -35,33 +24,28 @@ class App extends React.Component {
         body = response.data.observations;
         this.setState({ lobsterData: body });
         console.log(response.data.observations);
-        let data = response.data.observations;
-        let dates = "";
-        let price = "";
-        let i;
-        for (i = 0; i < data.length; i++) {
-          dates += data[i].date + ",";
-          price += data[i].value + ",";
-        }
-
-        this.setState({ dates: dates });
-        this.setState({ dates: this.state.dates.split(",") });
-        this.setState({ price: price });
-        this.setState({ price: this.state.price.split(",") });
+        let newArray = this.state.prices;
+        let monthsArray = this.state.dates;
+        this.state.lobsterData.forEach((month) => {
+          let value = parseFloat(month.value);
+          newArray.push(value);
+          monthsArray.push(month.date);
+        });
+        this.setState({ prices: newArray });
+        this.setState({ dates: monthsArray });
       })
       .catch((error) => console.error("Unable to get items.", error));
   }
 
   render() {
-    const { lobsterData } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div className="App-intro">
             <h2>Lobster Prices</h2>
-            <Line data={this.state.data} />
-            {lobsterData.map((array) => (
+
+            {this.state.lobsterData.map((array) => (
               <div key={array.date}>
                 {array.date} <span>value: </span>
                 {array.value}
