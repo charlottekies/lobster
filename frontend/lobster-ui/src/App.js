c40607af98a40471bf685ab4a3942c2f8eb48a9e
Charlotte Kies <chopekies@gmail.com>
2022-06-25 16:05:52 -0400

    rendered all lobster data on the screen

--- frontend/lobster-ui/src/App.js
+++ frontend/lobster-ui/src/App.js
@@ -2,7 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import axios from "axios";
-// import { Line } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 const http = axios.create({
   baseURL: "http://localhost:8080",
@@ -13,7 +15,8 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       isLoading: true,
-      data: {},
+      lobsterData: {},
+      data: "",
       dates: [],
       prices: [],
       pokemon: "my pokemon",
@@ -27,14 +30,46 @@ export default class App extends React.Component {
     //   this.setState({ isLoading: false });
     // });
     http.get("/lobsters/historical-price-data").then((response) => {
-      console.log(response);
-      this.setState({ lobsterData: response.data });
+      this.setState({ lobsterData: response.data.observations });
+      this.setData(response.data.observations);
       this.setState({ isLoading: false });
     });
   }
 
+  setData(data) {
+    let value = "";
+    let values = [];
+    let dates = [];
+    data.forEach((month) => {
+      if (month.value !== ".") {
+        value = parseFloat(month.value);
+        values.push(value);
+      } else {
+        values.push(".");
+      }
+      dates.push(month.date);
+    });
+    this.setState({ dates: dates });
+    this.setState({ prices: values });
+
+    this.setState({
+      data: {
+        labels: this.state.dates,
+        datasets: [
+          {
+            label: "First dataset",
+            data: this.state.prices,
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)",
+          },
+        ],
+      },
+    });
+  }
+
   render() {
-    const { isLoading, lobsterData } = this.state;
+    const { isLoading } = this.state;
 
     if (isLoading) {
       return <div className="App">Loading...</div>;
@@ -43,27 +78,8 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <h1>Lobster Data</h1>
-        {/* <img alt={pokemon.name} src={pokemon.sprites.front_default} /> */}
-        {this.state.lobsterData.observations[0].value}
+        <Line data={this.state.data} />
       </div>
     );
   }
 }
-
-// this.setState(
-//   {
-//     data: {
-//       labels: this.state.dates,
-//       datasets: [
-//         {
-//           label: "First dataset",
-//           data: this.state.prices,
-//           fill: true,
-//           backgroundColor: "rgba(75,192,192,0.2)",
-//           borderColor: "rgba(75,192,192,1)",
-//         },
-//       ],
-//     },
-//   },
-//   []
-// );
