96802d584616676f72e14dddd9ae67a98c495348
Charlotte Kies <chopekies@gmail.com>
2022-07-07 19:15:00 -0400

    Added JwtRequestFilter and DetailService code

--- kies/src/main/java/com/charlotte/kies/security/JwtUserDetailService.java
+++ kies/src/main/java/com/charlotte/kies/security/JwtUserDetailService.java
@@ -1,4 +1,20 @@
-package com.charlotte.kies.security;
 
-public class JwtUserDetailService {
+package com.charlotte.kies.security;
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class JwtUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("".equals(username)) {
+            return new User("", "",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
 }
