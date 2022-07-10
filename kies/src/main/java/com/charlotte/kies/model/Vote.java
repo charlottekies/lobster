29d3b0cb6a5646131f327cce6adb94cc493ecaca
Charlotte Kies <chopekies@gmail.com>
2022-07-10 16:24:57 -0400

    Created and updated vote model class

--- kies/src/main/java/com/charlotte/kies/model/Vote.java
+++ kies/src/main/java/com/charlotte/kies/model/Vote.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity(name = "user_vote")
@@ -22,4 +23,54 @@ public class Vote {
     @Column(name = "user_vote_id", nullable = false)
     private Long userVoteId;
 
+    @Column(name = "vote_date", nullable = false)
+    private Date voteDate;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "vote", nullable = false)
+    private Float vote;
+
+    public Vote(Long userVoteId, Date voteDate, Long userId, Float vote) {
+        this.userVoteId = userVoteId;
+        this.voteDate = voteDate;
+        this.userId = userId;
+        this.vote = vote;
+    }
+
+    public Vote() {
+    }
+
+    public Long getUserVoteId() {
+        return userVoteId;
+    }
+
+    public void setUserVoteId(Long userVoteId) {
+        this.userVoteId = userVoteId;
+    }
+
+    public Date getVoteDate() {
+        return voteDate;
+    }
+
+    public void setVoteDate(Date voteDate) {
+        this.voteDate = voteDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Float getVote() {
+        return vote;
+    }
+
+    public void setVote(Float vote) {
+        this.vote = vote;
+    }
 }
